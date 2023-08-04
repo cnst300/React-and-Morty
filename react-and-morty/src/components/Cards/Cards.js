c60d96e0506fb101c807bf89468ad9be60a85def
Constantin Jenaru <111741263+cnst300@users.noreply.github.com>
2023-08-04 14:18:37 +0300

    Add files via upload

---
+++ react-and-morty/src/components/Cards/Cards.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Cards.module.scss";
+import css from "./Cards.module.css";
+
+const Cards = ({ results, page }) => {
+  let display;
+  // console.log(results);
+  if (results) {
+    display = results.map((index) => {
+      let { id, name, image, species, status } = index;
+      return (
+        <Link
+        style={{textDecoration: "none"}}
+          to={`${page}${id}`}
+          key={id}
+          className="col-lg-4 col-md-6 col-12 mb-4 position-relative text-dark"
+        >
+          <div className={`${css.cards} ${css.shadow} d-flex flex-column justify-content-center`}>
+            <img src={image} alt="" className={`${styles.img} img-fluid`} />
+            <div style={{ padding: "20px" }} className="content">
+              <div className="fs-4 fw-bold mb-4">{name}</div>
+              <div className="fs-6  mb-3">Species: {species}</div>
+            </div>
+          </div>
+          {status === "Dead" ? (
+            <div
+              className={`${styles.badge} position-absolute badge bg-danger`}
+            >
+              {status}
+            </div>
+          ) : status === "Alive" ? (
+            <div
+              className={`${styles.badge} position-absolute badge bg-success`}
+            >
+              {status}
+            </div>
+          ) : (
+            <div
+              className={`${styles.badge} position-absolute badge bg-secondary`}
+            >
+              {status}
+            </div>
+          )}
+        </Link>
+      );
+    });
+  } else {
+    display = "No charachers Found :/";
+  }
+
+  return <>{display}</>;
+};
+
+export default Cards;
